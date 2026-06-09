a2f65aff6fc7d0bbc39c86dc2962f564d7fc5802
Sébastien Hensgen <24550538+sebhmg@users.noreply.github.com>
2026-06-08 21:22:10 -0400

    setup to publish package to Mira artifactory from fork

--- setup.py
+++ setup.py
@@ -60,7 +60,8 @@ setup(
     use_scm_version={
             "root"            : ".",
             "relative_to"     : __file__,
-            "write_to"        : path.join("gempy_engine", "_version.py"),
+            "version_file"    : path.join("gempy_engine", "_version.py"),
+            "write_to"        : "_version.txt",
             "fallback_version": "3.0.0"
     },
 )
